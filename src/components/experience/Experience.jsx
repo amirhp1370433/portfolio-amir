2a510ff2779dd1119a1eb50556a8d8197ff44a99
amirhp1370433 <amirhp1370433@gmail.com>
2022-08-28 12:16:14 +0430

    ok

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,101 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 const Experience = () => {
   return (
-    <section id='Experience'>Experience</section>
+    <section id='Experience'>
+       <h5>
+        what skills i have
+       </h5>
+       <h2>
+        my experience
+       </h2>
+       <div className="container experience__container">
+          <div className="experience__frontend">
+            <h3>
+              front-end experience
+            </h3>
+            <div className="experience__content">
+              <article className='experince__details'>
+                <BsPatchCheckFill/>
+                <h4>
+                  HTML
+                </h4>
+                
+              </article>
+              <article className='experince__details'>
+                <BsPatchCheckFill/>
+                <h4>
+                  CSS
+                </h4>
+ 
+              </article>
+              <article className='experince__details'>
+                <BsPatchCheckFill/>
+                <h4>
+                  JAVASCRIPT
+                </h4>
+                
+              </article>
+              <article className='experince__details'>
+                <BsPatchCheckFill/>
+                <h4>
+                  TAILWIND CSS
+                </h4>
+                
+              </article>
+              <article className='experince__details'>
+                <BsPatchCheckFill/>
+                <h4>
+                  REACT.JS
+                </h4>
+                
+              </article>
+              <article className='experince__details'>
+                <BsPatchCheckFill/>
+                <h4>
+                  AXIOS
+                </h4> 
+              </article>
+            </div>
+          </div>
+          <div className="experience__backend">
+          <h3>
+              back-end experience
+            </h3>            
+            <div className="experience__content">
+              <article className='experince__details'>
+                <BsPatchCheckFill/>
+                <h4>
+                  NODE.JS
+                </h4>
+                
+              </article>
+              <article className='experince__details'>
+                <BsPatchCheckFill/>
+                <h4>
+                  REST_API
+                </h4>
+                
+              </article>
+              <article className='experince__details'>
+                <BsPatchCheckFill/>
+                <h4>
+                  MONGO DB
+                </h4>
+                
+              </article>
+              <article className='experince__details'>
+                <BsPatchCheckFill/>
+                <h4>
+                  EXPRESS
+                </h4>
+                
+              </article>
+    </div>
+          </div>
+       </div>
+    </section>
   )
 }
 
