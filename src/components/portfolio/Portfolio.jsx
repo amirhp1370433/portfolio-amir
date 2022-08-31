5f0cc1446b781d59c9ab1930f7d911fc03e844d2
amirhp1370433 <amirhp1370433@gmail.com>
2022-08-31 11:55:47 +0430

    ok

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 import './portfolio.css'
-const Portfolio = () => {
+import data from './portfolioData'
+ const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>
+        my recent works
+      </h5>
+      <h2>
+        portfolio
+      </h2>
+      <div className="container portfolio__container">
+        {
+          data.map(( i , index)=>{
+            return(
+              <article key={index} className='portfolio__item'>
+                <div className="portfolio__item-image">
+                  <img src={i.image} alt={i.titel} className=''/>
+                </div>
+                  <h3>
+                    {i.titel}
+                  </h3>
+                  <div className="portfolio__item-cta">
+                    <a href={i.github} className='btn' target="_blank">github</a>
+                    <a href={i.website} className='btn btn-primary' target='_blank'>live Demo</a>
+                  </div>
+              </article>
+            )
+          })
+        }
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/testimonials/test.js
@@ -0,0 +1,37 @@
+import avatar1 from '../../assets/avatar1.jpg'
+import avatar2 from '../../assets/avatar2.jpg'
+import avatar3 from '../../assets/avatar3.jpg'
+import avatar4 from '../../assets/avatar4.jpg'
+
+const data = [
+    {
+        id:1 , 
+        image:avatar1,
+        github:'https://github.com/amirhp1370433/portfolio-website-main',
+        website:'http://87.107.167.222/#work',
+        titel:'website'
+    },
+    {
+        id:2 , 
+        image:avatar2,
+        github:'https://github.com/amirhp1370433/portfolio-website-main',
+        website:'http://87.107.167.222/#work',
+        titel:'website'
+    },
+    {
+        id:3 , 
+        image:avatar3,
+        github:'https://github.com/amirhp1370433/portfolio-website-main',
+        website:'http://87.107.167.222/#work',
+        titel:'website'
+    },
+    {
+        id:4 , 
+        image:avatar4,
+        github:'https://github.com/amirhp1370433/portfolio-website-main',
+        website:'http://87.107.167.222/#work',
+        titel:'website'
+    }
+]
+
+export default data
