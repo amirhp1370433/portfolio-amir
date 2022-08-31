37f2a8d912c7247b3ba7e8e136c6b0d204e17205
amirhp1370433 <amirhp1370433@gmail.com>
2022-08-31 11:55:09 +0430

    ok

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import './testimonials.css'
+import avtr from '../../assets/'
 const Testimonials = () => {
   return (
-    <section id='Testimonials'>Testimonials</section>
+    <section id='Testimonials'>
+      <h5>
+        review from clients
+      </h5>
+      <h2>
+        Testimonials
+      </h2>
+      <div className="container testimonials__container">
+        <article className='testimonials'>
+          <div className="client__avatar">
+
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
